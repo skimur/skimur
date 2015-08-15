de6532f7dbbe88aeacf435a2da5b83e21992a590
Paul Knopf <theonlylawislove@gmail.com>
2015-08-15 05:49:56 -0400

    Prevent processing votes for users who are banned from the sub.

--- src/Subs/Subs.Worker/VoteCommandHandler.cs
+++ src/Subs/Subs.Worker/VoteCommandHandler.cs
@@ -23,18 +23,21 @@ namespace Subs.Worker
         private readonly IVoteService _voteService;
         private readonly ICommentService _commentService;
         private readonly IEventBus _eventBus;
+        private readonly ISubUserBanService _subUserBanService;
 
         public VoteCommandHandler(IMembershipService membershipService,
             IPostService postService,
             IVoteService voteService,
             ICommentService commentService,
-            IEventBus eventBus)
+            IEventBus eventBus,
+            ISubUserBanService subUserBanService)
         {
             _membershipService = membershipService;
             _postService = postService;
             _voteService = voteService;
             _commentService = commentService;
             _eventBus = eventBus;
+            _subUserBanService = subUserBanService;
         }
 
         public void Handle(CastVoteForPost command)
@@ -48,6 +51,10 @@ namespace Subs.Worker
 
             if (post == null)
                 return;
+
+            if (!user.IsAdmin)
+                if (_subUserBanService.IsUserBannedFromSub(post.SubId, user.Id))
+                    return;
 
             if (command.VoteType.HasValue)
                 _voteService.VoteForPost(post.Id, user.Id, command.IpAddress, command.VoteType.Value, command.DateCasted);
@@ -68,6 +75,15 @@ namespace Subs.Worker
 
             if (comment == null)
                 return;
+
+            var post = _postService.GetPostById(comment.PostId);
+
+            if (post == null)
+                return;
+
+            if (!user.IsAdmin)
+                if (_subUserBanService.IsUserBannedFromSub(post.SubId, user.Id))
+                    return;
 
             if (command.VoteType.HasValue)
                 _voteService.VoteForComment(comment.Id, user.Id, command.IpAddress, command.VoteType.Value, command.DateCasted);
