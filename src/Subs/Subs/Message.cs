75dbe99716d5069e2588a377e635c6f5bc1d816f
Paul Knopf <theonlylawislove@gmail.com>
2015-09-05 14:26:33 -0400

    Created the migration for our messages table.

---
+++ src/Subs/Subs/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subs
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        
+        public DateTime DateCreated { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        public Guid? FirstMessage { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public string AuthorIp { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public Guid? ToUser { get; set; }
+
+        public Guid? ToSub { get; set; }
+
+        public Guid? FromSub { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
