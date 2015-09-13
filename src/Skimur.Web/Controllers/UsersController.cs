a7cd8d62e6c53f84f73d1681a5f3c40ebe712e3d
Paul Knopf <theonlylawislove@gmail.com>
2015-09-13 07:09:37 -0400

    Added landing page for user profile. I am waiting to implement this as a part of the next milestone so we can release an alpha sooner. #2

--- src/Skimur.Web/Controllers/UsersController.cs
+++ src/Skimur.Web/Controllers/UsersController.cs
@@ -4,14 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Membership.Services;
+using Skimur.Web.Models;
+using Subs.ReadModel;
+using Subs.Services;
 
 namespace Skimur.Web.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly IMembershipService _membershipService;
+        private readonly ISubDao _subDao;
+
+        public UsersController(IMembershipService membershipService,
+            ISubDao subDao)
+        {
+            _membershipService = membershipService;
+            _subDao = subDao;
+        }
+
         public ActionResult User(string userName)
         {
-            return View();
+            var user = _membershipService.GetUserByUserName(userName);
+
+            if(user == null)
+                throw new NotFoundException();
+            
+            var model = new UserViewModel();
+            model.User = user;
+            
+            var moderatedSubs = _subDao.GetSubsModeratoredByUser(user.Id);
+
+            if (moderatedSubs.Count > 0)
+            {
+                model.IsModerator = true;
+                model.ModeratingSubs = _subDao.GetSubsByIds(moderatedSubs).Select(x => x.Name).ToList();
+            }
+
+            return View(model);
         }
     }
 }
---
+++ src/Skimur.Web/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Membership;
+
+namespace Skimur.Web.Models
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+
+        public bool IsModerator { get; set; }
+
+        public List<string> ModeratingSubs { get; set; } 
+    }
+}
