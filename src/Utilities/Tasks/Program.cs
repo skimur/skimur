0cf03531c8d4f2f6f5f31887889656231e29b59c
Paul Knopf <theonlylawislove@gmail.com>
2015-10-17 02:25:19 -0400

    Added a method to delete a sub.

--- src/Utilities/Tasks/Program.cs
+++ src/Utilities/Tasks/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Membership.Services;
 using PowerArgs;
+using ServiceStack.OrmLite;
 using Skimur;
+using Subs;
 using Subs.Services;
 
 namespace Tasks
@@ -96,6 +99,37 @@ namespace Tasks
             }
 
 
+        }
+
+        [ArgActionMethod, ArgDescription("Delete a sub")]
+        public void DeleteSub(string subName)
+        {
+            var connectionProvider = SkimurContext.Resolve<IDbConnectionProvider>();
+
+            var sub = connectionProvider.Perform(conn => conn.Single<Sub>(x => x.Name.ToLower() == subName.ToLower()));
+            if (sub == null)
+            {
+                Console.WriteLine("No sub with that name.");
+                return;
+            }
+
+            foreach (var post in connectionProvider.Perform(conn => conn.Select<Post>(x => x.SubId == sub.Id)))
+            {
+                connectionProvider.Perform(conn => conn.Delete<Vote>(x => x.PostId == post.Id));
+                connectionProvider.Perform(conn => conn.Delete<Post>(x => x.Id == post.Id));
+            }
+
+            foreach (var comment in connectionProvider.Perform(conn => conn.Select<Comment>(x => x.SubId == sub.Id)))
+            {
+                connectionProvider.Perform(conn => conn.Delete<Vote>(x => x.CommentId == comment.Id));
+                connectionProvider.Perform(conn => conn.Delete<Comment>(x => x.Id == comment.Id));
+            }
+
+            connectionProvider.Perform(conn => conn.Delete<Moderator>(x => x.SubId == sub.Id));
+            connectionProvider.Perform(conn => conn.Delete<ModeratorInvite>(x => x.SubId == sub.Id));
+            connectionProvider.Perform(conn => conn.Delete<SubCss>(x => x.SubId == sub.Id));
+            connectionProvider.Perform(conn => conn.Delete<SubUserBan>(x => x.SubId == sub.Id));
+            connectionProvider.Perform(conn => conn.Delete<Sub>(x => x.Id == sub.Id));
         }
     }
 
