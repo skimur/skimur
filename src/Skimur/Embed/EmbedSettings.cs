0e897681d579e81fa0bcc52cf93ce1fe1873f281
Paul Knopf <theonlylawislove@gmail.com>
2015-11-16 01:41:09 -0500

    Disabling embedlur by default.

--- src/Skimur/Embed/EmbedSettings.cs
+++ src/Skimur/Embed/EmbedSettings.cs
@@ -8,12 +8,6 @@ namespace Skimur.Embed
 {
     public class EmbedSettings : ISettings
     {
-        public EmbedSettings()
-        {
-            EmbedlurEnabled = true;
-            EmbedlurEndpoint = "http://localhost:35838/";
-        }
-
         public bool EmbedlurEnabled { get; set; }
 
         public string EmbedlurEndpoint { get; set; }
