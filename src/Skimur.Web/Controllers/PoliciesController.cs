8272aa72a57351d36cd64fad3a8c0b8a13ebfdc7
Paul Knopf <theonlylawislove@gmail.com>
2015-12-15 04:41:11 -0500

    Added back our policies

---
+++ src/Skimur.Web/Controllers/PoliciesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skimur.Web.Controllers
+{
+    public class PoliciesController : BaseController
+    {
+        public ActionResult PrivacyPolicy()
+        {
+            ViewBag.ManageNavigationKey = "privacypolicy";
+            return View();
+        }
+
+        public ActionResult UserAgreement()
+        {
+            ViewBag.ManageNavigationKey = "useragreement";
+            return View();
+        }
+
+        public ActionResult ContentPolicy()
+        {
+            ViewBag.ManageNavigationKey = "contentpolicy";
+            return View();
+        }
+    }
+}
