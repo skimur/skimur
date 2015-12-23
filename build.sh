69902d43da16375d17ea5e1db4d69b8d3e741c43
Paul Knopf <theonlylawislove@gmail.com>
2015-12-23 01:41:22 -0500

    Added sake build system.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if test `uname` = Darwin; then
-    cachedir=~/Library/Caches/KBuild
+    cachedir=~/Library/Caches/SakeBuild
 else
     if [ -z $XDG_DATA_HOME ]; then
         cachedir=$HOME/.local/share
@@ -25,19 +25,8 @@ if test ! -e .nuget; then
 fi
 
 if test ! -d packages/Sake; then
-    mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+    mono .nuget/nuget.exe install SakeExtensions -ExcludeVersion -o packages -nocache -pre
     mono .nuget/nuget.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
 fi
 
-if ! type dnvm > /dev/null 2>&1; then
-    source packages/KoreBuild/build/dnvm.sh
-fi
-
-if ! type dnx > /dev/null 2>&1 || [ -z "$SKIP_DNX_INSTALL" ]; then
-    dnvm install latest -runtime coreclr -alias default
-    dnvm install default -runtime mono -alias default
-else
-    dnvm use default -runtime mono
-fi
-
-mono packages/Sake/tools/Sake.exe -I packages/KoreBuild/build -f makefile.shade "$@"
+mono packages/Sake/tools/Sake.exe -I packages/SakeExtensions/build -f makefile.shade "$@"
