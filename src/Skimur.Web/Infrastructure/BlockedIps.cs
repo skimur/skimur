5788cec869a3e78b736817d5cd3886f797ecf8a7
Paul Knopf <theonlylawislove@gmail.com>
2015-12-24 00:00:59 -0500

    Added the ip blocking middleware.

---
+++ src/Skimur.Web/Infrastructure/BlockedIps.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skimur.Web.Infrastructure
+{
+    public class BlockedIps : ISettings
+    {
+        public List<string> Ips { get; set; }
+    }
+}
--- src/Skimur.Web/Startup.cs
+++ src/Skimur.Web/Startup.cs
@@ -68,6 +68,8 @@ namespace Skimur.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseMiddleware<IpBlockerMiddleware>();
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
