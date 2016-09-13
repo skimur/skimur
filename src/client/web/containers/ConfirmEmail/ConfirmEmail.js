c4c51ac1b77a362af92c4e514cdc3229defd54b9
Paul Knopf <pauldotknopf@gmail.com>
2016-09-13 04:10:33 -0400

    Display email change messages on landing page.

--- src/client/web/containers/ConfirmEmail/ConfirmEmail.js
+++ src/client/web/containers/ConfirmEmail/ConfirmEmail.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import { Link } from 'react-router';
 
 @inject('store')
 @observer
 export default class ConfirmEmail extends Component {
   render() {
+    const {
+      success,
+      change
+    } = this.props.store.viewBag;
     return (
       <div>
+        {!success &&
+          <div>
+            <h1 className="text-danger">Error.</h1>
+            <h2 className="text-danger">An error occurred while processing your request.</h2>
+          </div>
+        }
+        {success &&
+          <div>
+            {change &&
+              <div>
+                <h2>Change email</h2>
+                <p>
+                  Your email was succesfully changed.
+                </p>
+              </div>
+            }
+            {!change &&
+              <div>
+                <h2>Confirm email</h2>
+                <p>
+                  Thank you for confirming your email.
+                  Please <Link to="/login">Click here to Log in</Link>.
+                </p>
+              </div>
+            }
+          </div>
+        }
       </div>
     );
   }
